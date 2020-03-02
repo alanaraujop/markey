61c9b96abe945cc95c96776986e00ea59ad8f1e5
Alan Araujo <alan.techsafe@hotmail.com>
2020-03-02 02:47:46 -0300

    Test

---
+++ precache-manifest.418fe0998cba177bda5d30c4c22044bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d9f53492919825b05fa6be13811535",
+    "url": "/markey/index.html"
+  },
+  {
+    "revision": "f7fded43f98b83be57ee",
+    "url": "/markey/static/js/2.7012e755.chunk.js"
+  },
+  {
+    "revision": "8c1e0d6b047a65655d0b7ff12377370e",
+    "url": "/markey/static/js/2.7012e755.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e807e56e31f280538242",
+    "url": "/markey/static/js/main.c93b2443.chunk.js"
+  },
+  {
+    "revision": "f5951098e897bf28725a",
+    "url": "/markey/static/js/runtime-main.69e14e58.js"
+  },
+  {
+    "revision": "2a1b78e28c6bd79594004f93efecbf6b",
+    "url": "/markey/static/media/avatar.2a1b78e2.svg"
+  },
+  {
+    "revision": "edb65c0b2a85cddd6ea40fba7f7d0816",
+    "url": "/markey/static/media/drmarkey-horizontal-azul.edb65c0b.svg"
+  },
+  {
+    "revision": "73dfce18fa173d472a8f90540776763a",
+    "url": "/markey/static/media/drmarkey-horizontal-branca.73dfce18.svg"
+  },
+  {
+    "revision": "1b317e4a4912516328c1b490b4588576",
+    "url": "/markey/static/media/drmarkey-vertical-azul.1b317e4a.svg"
+  },
+  {
+    "revision": "7d019352328a7d9f04ce40729e1a1698",
+    "url": "/markey/static/media/drmarkey-vertical-branca.7d019352.svg"
+  }
+]);
